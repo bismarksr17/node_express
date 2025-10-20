de5cc3880cbe04209937750cd2987274ce54e725
Bismark Socompi <bismark.sr.17@gmail.com>
2025-10-20 06:05:13 -0400

    update 20-10-2025

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json()); //soporte para json
+app.use(bodyParser.urlencoded({ extended: true })); //soporte para formData (datos de formulario)
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,6 +16,24 @@ app.get('/', (req, res) => {
         <p>Corre en el puerto ${PORT}</p>
         `);
 });
+
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    res.send(`Mostrar información de usuario con ID = ${userId}`);
+});
+
+app.get('/search', (req, res) => {
+    const terms = req.params.termino || 'No especificado';
+    const category = req.params.categoria || 'Todas';
+
+    res.send(`
+        <h2>Resultado de búsqueda</h2>
+        <p>Término: ${terms}</p>
+        <p>Categoría: ${category}</p>
+        `);
+});
+//url: http://localhost:3000/search?termino=expressjs&categoria=nodejs
+
 
 app.listen(PORT, () => {
     console.log(`App de ejemplo escuchando en el puerto ${PORT}`)
