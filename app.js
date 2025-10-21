86880a11ebc8329e520ad6fd0084d6ec920cb39d
Bismark Socompi <bismark.sr.17@gmail.com>
2025-10-21 07:06:28 -0400

    update 21-10-2025

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const app = express();
 app.use(bodyParser.json()); //soporte para json
 app.use(bodyParser.urlencoded({ extended: true })); //soporte para formData (datos de formulario)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 
+//ruta raíz
 app.get('/', (req, res) => {
     //res.send('Hola mundo..!!');
     res.send(`
@@ -17,11 +18,15 @@ app.get('/', (req, res) => {
         `);
 });
 
+//ruta dinámica
+//URL: http://localhost:3000/users/3
 app.get('/users/:id', (req, res) => {
     const userId = req.params.id;
     res.send(`Mostrar información de usuario con ID = ${userId}`);
 });
 
+//obtención de datos por parámetros
+//URL: http://localhost:3000/search?termino=expressjs&categoria=nodejs
 app.get('/search', (req, res) => {
     const terms = req.params.termino || 'No especificado';
     const category = req.params.categoria || 'Todas';
@@ -32,9 +37,35 @@ app.get('/search', (req, res) => {
         <p>Categoría: ${category}</p>
         `);
 });
-//url: http://localhost:3000/search?termino=expressjs&categoria=nodejs
 
+//recepción de datos de formulario
+app.post('/form', (req, res) => {
+    const name = req.body.nombre || 'Anónimo';
+    const email = req.body.email || 'No proporcionado';
+    res.json({
+        message : 'Datos recibidos',
+        data: {
+            name, 
+            email
+        }
+    });
+});
 
+//recepción de datos de json
+app.post('/api/data', (req, res) => {
+    const data = req.body;
+
+    if (!data || Object.keys(data).length == 0) {
+        return res.status(400).json({ error : 'No se recibieron datos' })
+    }
+
+    res.status(201).json({
+        message : 'Datos JSON recibido',
+        data
+    });
+})
+
+//arranque de servidor
 app.listen(PORT, () => {
     console.log(`App de ejemplo escuchando en el puerto ${PORT}`)
 });
