7b115c83d8fc3e4ef80306c6548723dd7011d4b1
Bismark Socompi <bismark.sr.17@gmail.com>
2025-10-22 07:14:25 -0400

    update 10-22-2025

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+//para acceso a las variables de entorno
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 
+//uso de fileSystem 
+const fs = require('fs');
+const path = require('path');
+const usersFilePath = path.join(__dirname, 'users.json');
+
+//instancia de express
 const app = express();
+
+//declaración de middleware para body-parser
 app.use(bodyParser.json()); //soporte para json
 app.use(bodyParser.urlencoded({ extended: true })); //soporte para formData (datos de formulario)
 
+//lectura de puerto desde variables de entorno
 const PORT = process.env.PORT || 3005;
 
 //ruta raíz
@@ -64,6 +74,37 @@ app.post('/api/data', (req, res) => {
         data
     });
 })
+
+// implementación de método GET de usuarios
+app.get('/users', (req, res) => {
+    //lectura de archivo users.json
+    fs.readFile(usersFilePath, 'utf-8', (err, data) => {
+        //validar si existe algún error
+        if (err) {
+            return res.status(500).json({error : 'Error con la conexión de datos'})
+        }
+        //parsea la data a JSON
+        const users = JSON.parse(data);
+        res.json(users)
+    })
+});
+
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+    fs.readFile(usersFilePath, 'utf-8', (err, data) => {
+        if(err){
+            return res.status(500).json({ error : 'Error con conexión de datos'});
+        }
+        const users = JSON.parse(data)
+        users.push(newUser);
+        fs.writeFile(usersFilePath, JSON.stringify(users, null, 2), (err) => {
+            if (err) {
+                return res.status(500).json({error : 'Error al guardar el usuario'})
+            }
+            res.status(201).json(newUser);
+        });
+    });
+});
 
 //arranque de servidor
 app.listen(PORT, () => {
