67c69a6d914e8ade7de2053138d4516904551ab1
Bismark Socompi <bismark.sr.17@gmail.com>
2025-11-04 18:21:01 -0400

    update 4-11-2025

---
+++ src/controllers/adminController.js
@@ -0,0 +1,34 @@
+const { 
+    createTimeBlocksService, 
+    listReservationsService 
+} = require('../services/adminService');
+
+const createTimeBlock = async (req, res) => {
+    if(req.user.role !== 'ADMIN') {
+        return res.stattus(403).json({error : 'Accces denied'});
+    }
+
+    const { startTime, endTime } = req.body();
+    try {
+        const newTimeBlock = await createTimeBlocksService(startTime, endTime);
+        res.status(201).json(newTimeBlock);
+    } catch (error) {
+        res.status(500).json({error : 'Error creating time blockss'})
+    }
+};
+
+const listReservations = async (req, res) => {
+    if(req.user.role !== 'ADMIN') {
+        return res.stattus(403).json({error : 'Accces denied'});
+    }
+
+    try {
+        const reservations = await listReservationsService();
+        res.json(reservations);
+    }
+    catch (error) {
+        res.status(500).json({error : 'Error fetching reservations'});
+    }
+}
+
+module.exports = { createTimeBlock, listReservations }
